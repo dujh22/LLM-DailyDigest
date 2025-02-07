5848847b9ce21d2d8fd7237c4b40cf895b095b58
杜晋华 <dujinhua2021@163.com>
2025-02-07 18:56:10 +0800

    1

--- tools/arx_dairy_summarizer_and_to_github.sh
+++ tools/arx_dairy_summarizer_and_to_github.sh
@@ -4,21 +4,20 @@
 project_dir="/Users/djh/Documents/GitHub/LLM-DailyDigest"
 
 # 获取时间戳,格式为2025_02_07_11_10_39
-# timestamp=$(date +'%Y_%m_%d_%H_%M_%S')
-timestamp="2025_02_07_17_37_16"
+timestamp=$(date +'%Y_%m_%d_%H_%M_%S')
 # 打印时间戳
 echo "Timestamp: $timestamp"
 
-# # 下载数据
-# filename="arxiv_papers_$timestamp.csv"
-# python arx.py --filename $filename
-# # 下载结束后打印
-# echo "Downloaded data"
+# 下载数据
+filename="arxiv_papers_$timestamp.csv"
+python arx.py --filename $filename
+# 下载结束后打印
+echo "Downloaded data"
 
-# # 处理数据
-# python arx_batch_to_ch.py --time $timestamp
-# # 处理结束后打印
-# echo "Processed data"
+# 处理数据
+python arx_batch_to_ch.py --time $timestamp
+# 处理结束后打印
+echo "Processed data"
 
 # 获取前一天的时间戳 (YYYY-MM-DD)
 yesterday=$(date -v-1d +'%Y-%m-%d')
